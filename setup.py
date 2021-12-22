724d5754fbd9eecca56a68dd9edb0218f4e52bc8
Runningchauncey <ericleecx@foxmail.com>
2021-12-22 11:33:51 +0100

    first commit on this branch

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=['araig_gui'],
+    packages=['araig_gui_stand_alone'],
     package_dir={'': 'src'},
     requires=['std_msgs', 'roscpp'],
     scripts=['scripts/araig_gui']
